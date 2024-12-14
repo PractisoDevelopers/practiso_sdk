3786385be0d6ce901be68eb8660a2d7345d00b78
zhufucdev <zhufuzhufu1@gmail.com>
2024-12-14 08:31:16 +0800

    :sparkles: more custom parameters

--- practiso_sdk/google/ai.py
+++ practiso_sdk/google/ai.py
@@ -10,45 +10,51 @@ from practiso_sdk.build import VectorizeAgent
 
 class GeminiAgent(VectorizeAgent):
     __api_key: str
+    __model: genai.GenerativeModel
 
-    # noinspection PyTypeChecker
-    __model = genai.GenerativeModel(
-        model_name="gemini-2.0-flash-exp",
-        generation_config={
-            'temperature': 1,
-            'top_p': 0.95,
-            'top_k': 40,
-            'max_output_tokens': 8192,
-            'response_schema': content.Schema(
-                type=content.Type.OBJECT,
-                enum=[],
-                required=['dimensions'],
-                properties={
-                    'dimensions': content.Schema(
-                        type=content.Type.ARRAY,
-                        items=content.Schema(
-                            type=content.Type.OBJECT,
-                            enum=[],
-                            required=['name', 'intensity'],
-                            properties={
-                                'name': content.Schema(
-                                    type=content.Type.STRING,
-                                ),
-                                'intensity': content.Schema(
-                                    type=content.Type.NUMBER,
-                                ),
-                            },
-                        ),
-                    ),
-                },
-            ),
-            'response_mime_type': 'application/json',
-        },
-        system_instruction='You are a student tagging quizzes from your collections. You always look at the XML presentation of a quiz and determine what category the quiz falls into and how much so, rating the intensity from 0 to 1. You always name the category in the language the quiz is written in, and break the categories into several small knowledge points. Here comes your first quiz.',
-    )
-
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, temperature: float = 0, top_p: float = 0.95, top_k: float = 40):
         self.__api_key = api_key
+        # noinspection PyTypeChecker
+        self.__model = genai.GenerativeModel(
+            model_name="gemini-2.0-flash-exp",
+            generation_config={
+                'temperature': temperature,
+                'top_p': top_p,
+                'top_k': top_k,
+                'max_output_tokens': 8192,
+                'response_schema': content.Schema(
+                    type=content.Type.OBJECT,
+                    enum=[],
+                    required=['dimensions'],
+                    properties={
+                        'dimensions': content.Schema(
+                            type=content.Type.ARRAY,
+                            items=content.Schema(
+                                type=content.Type.OBJECT,
+                                enum=[],
+                                required=['name', 'intensity'],
+                                properties={
+                                    'name': content.Schema(
+                                        type=content.Type.STRING,
+                                    ),
+                                    'intensity': content.Schema(
+                                        type=content.Type.NUMBER,
+                                    ),
+                                },
+                            ),
+                        ),
+                    },
+                ),
+                'response_mime_type': 'application/json',
+            },
+            system_instruction='You are a student tagging quizzes from your collections. '
+                               'You always look at the XML presentation of a quiz and '
+                               'determine what category the quiz falls into and how much '
+                               'so, rating the intensity from 0 to 1. You always name the '
+                               'category in the language the quiz is written in, and break '
+                               'the categories into several small knowledge points. '
+                               'Here comes your first quiz.',
+        )
 
     async def get_dimensions(self, quiz: Quiz) -> set[Dimension]:
         genai.configure(api_key=self.__api_key)
